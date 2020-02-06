ec8112eb3c44111b3fb3f9a00b843d44d2145a66
wamakimaN <wamamoses@gmail.com>
2020-02-06 11:52:25 +0300

    add base template that all pages inherit styles from

--- pics/views.py
+++ pics/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-  return render(request, 'homepage.html') 
+  title = 'home'
+  return render(request, 'homepage.html', {"title":title}) 
