3c148b891f190d4fb95d80de06e53d9b8a860f11
wamakimaN <wamamoses@gmail.com>
2020-02-06 14:48:11 +0300

    remove buggy line of code

--- gallery/urls.py
+++ gallery/urls.py
@@ -22,7 +22,3 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('pics.urls'))
 ]
-
-
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
