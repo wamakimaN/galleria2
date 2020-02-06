2f7a41a2065c7802c01ba4882138b32570947f6f
wamakimaN <wamamoses@gmail.com>
2020-02-06 15:10:17 +0300

    add view function to wiee pictures

--- pics/views.py
+++ pics/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from .models import Picture,Category,Location
 
 # Create your views here.
 def home_page(request):
   title = 'home'
   
   pics = Picture.all_pics()
-  
+
   return render(request, 'homepage.html', {"title":title,"pics":pics})
