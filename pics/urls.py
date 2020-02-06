6bad572dd46a340d2e1c658b6c0bb95a60718a1d
wamakimaN <wamamoses@gmail.com>
2020-02-06 14:45:19 +0300

    registyer models and make migrations

--- pics/urls.py
+++ pics/urls.py
@@ -6,3 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.home_page, name='homePage')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
