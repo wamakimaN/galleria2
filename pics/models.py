5411895b16c7beb957f065ffc64e8420af6f227d
wamakimaN <wamamoses@gmail.com>
2020-02-06 12:23:20 +0300

    add models

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length =30)
+  description = models.CharField(max_length =200)
+
+  def __str__(self):
+        return self.name
+
+class Location(models.Model):
+  name = models.CharField(max_length=30)
+
+  def __str__(self):
+        return self.name
+
+
+class Picture(models.Model):
+  image = models.ImageField(upload_to = 'pictures/')
+  pic_name = models.CharField(max_length =30)
+  pic_desc =models.CharField(max_length =200)
+  category = models.ForeignKey(Category)
+  location = models.ForeignKey(Location)
+
+  def __str__(self):
+        return self.pic_name
